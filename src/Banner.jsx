a072132c8c25c5ddd5f2411701112e0ee2471de6
Tom Cohen <tomerc2010@cs.colman.ac.il>
2022-01-20 20:03:31 +0200

    second commit

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -25,7 +25,7 @@ function Banner(props) {
       <div className="banner__info">
         <h1>Get out and strech your imagination</h1>
         <h5>
-          Plan a different kinf of gateway to uncover the hidden gems near you.
+          Plan a different kind of gateway to uncover the hidden gems near you.
         </h5>
         <Button onClick={() => navigate("/search")}>Explore nearby</Button>
       </div>
